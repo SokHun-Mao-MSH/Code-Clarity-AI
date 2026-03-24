27386c4c3266b5fb33e343567e497c83a17be477
SokHun-Mao-MSH <mlyhour57@gmail.com>
2026-03-24 13:02:51 +0700

    Setup GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "/Code-Clarity-AI/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
